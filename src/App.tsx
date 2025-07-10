7e45ff0a59866e3db6b0e3a5ab7d31de89513969
Mike Linder <miky2493@gmail.com>
2025-07-10 13:49:25 -0300

    Container 'Body' criado e inserido em 'App.tsx'

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,21 @@ import NotFoundPage from './pages/notFound/NotFoundPage'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import Dashboard from './pages/dashboard/DashboardPage'
+import Body from './components/body/Body'
 
 function App() {
 
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<Licitacao />} />
-        <Route path="/detalhes/:numero" element={<DetalhesPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
+      <Body>
+        <Routes>
+          <Route path="/" element={<Licitacao />} />
+          <Route path="/detalhes/:numero" element={<DetalhesPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </Body>
       <Footer />
     </Router>
   )
