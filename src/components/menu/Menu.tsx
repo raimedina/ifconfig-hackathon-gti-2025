932ccfb9f2ad69557fe3cd103480c8d0d77132ca
ojuangoncalves <juan.gonca07@gmail.com>
2025-07-10 20:37:35 -0300

    criação do componente de menu

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,19 @@
+import "./Menu.css"
+
+function Menu() {
+    return (
+        <ul className="nav-menu">
+            <li className="nav-item">Receita</li>
+            <li className="nav-item">Despesa</li>
+            <li className="nav-item">Compras</li>
+            <li className="nav-item">Publicações Legais</li>
+            <li className="nav-item">Gestão de Pessoal</li>
+            <li className="nav-item">Recursos Físicos</li>
+            <li className="nav-item">Dados Abertos</li>
+            <li className="nav-item">Acesso à Informação</li>
+            <li className="nav-item">Emendas Parlamentares</li>
+        </ul>
+    )
+}
+
+export default Menu
